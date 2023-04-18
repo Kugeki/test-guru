933fc68e387eb3a38a86a0580e57e3798cc714b4
Kugeki <54151036+Kugeki@users.noreply.github.com>
2023-04-18 22:17:54 +0300

    feat: add time zone

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module TestGuru
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Moscow"
   end
 end
