be8c55f443a6504b2cb44cc285ddc016a194024b
Kugeki <54151036+Kugeki@users.noreply.github.com>
2023-06-13 17:51:54 +0300

    feat: add `#tests_with_level` to `User`

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :attempts
+
+  def tests_with_level(level)
+    Attempt.where(user: self).joins(:test).where(tests: { level: }).map(&:test).uniq
+  end
 end
