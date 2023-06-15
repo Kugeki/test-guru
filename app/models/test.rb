15acb39997499eb5eea56aae6f71fcf32ab72355
Kugeki <54151036+Kugeki@users.noreply.github.com>
2023-06-15 09:41:12 +0300

    feat: add association between `User` and `Test`

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,7 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: "User", optional: true
 
   has_many :attempts
+  has_many :attempted_users, through: :attempts, source: :user
 
   class << self
     def with_category(title)
